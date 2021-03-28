417126d3327a0487762de60510e809e505d83f9f
Jason Kohles <email@jasonkohles.com>
2021-03-28 18:08:48 -0400

    updates

---
+++ cfg/setup-machine.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+
+echo "$@"
+
+REPO="jasonk/dotfiles"
+BRANCH="master"
+
+# CFGURL="https://raw.githubusercontent.com/$REPO/$BRANCH/cfg"
+CFGURL="file:///Users/jason/cfg"
+
+if ! command -v brew 2>/dev/null 1>/dev/null; then
+  HBURL="https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
+  /bin/bash -c "$(curl -fsSL "$HBURL")"
+fi
+
+curl -fsSL "$CFGURL/Brewfile" | brew bundle --file=/dev/stdin
+
+export GHREPO="https://github.com/$REPO"
+if [ ! -d "$HOME/.cfg" ]; then
+  REPO="$GHREPO" curl -fsSL "$CFGURL/checkout.sh"
+fi
